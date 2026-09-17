1802c2eedca5023a1110ccb26ce13b1a5fd93ae9
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-09-17 10:01:11 +0200

    import Game component in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Game } from './game/game';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Game],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
