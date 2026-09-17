db8f9306dd85dbf5e4266be845506babe9b094ce
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-09-17 11:46:04 +0200

    add real data for pokemon types

---
+++ src/app/data/types.ts
@@ -0,0 +1,22 @@
+import { Type } from '../models/type';
+
+export const TYPES: Type[] = [
+  { name: 'normal',   color: '#9FA19F' },
+  { name: 'fire',     color: '#E62829' },
+  { name: 'water',    color: '#2980EF' },
+  { name: 'electric', color: '#FAC000' },
+  { name: 'grass',    color: '#3FA129' },
+  { name: 'ice',      color: '#3FD8FF' },
+  { name: 'fighting', color: '#FF8000' },
+  { name: 'poison',   color: '#9141CB' },
+  { name: 'ground',   color: '#915121' },
+  { name: 'flying',   color: '#81B9EF' },
+  { name: 'psychic',  color: '#EF4179' },
+  { name: 'bug',      color: '#91A119' },
+  { name: 'rock',     color: '#AFA981' },
+  { name: 'ghost',    color: '#704170' },
+  { name: 'dragon',   color: '#5060E1' },
+  { name: 'dark',     color: '#624D4E' },
+  { name: 'steel',    color: '#60A1B8' },
+  { name: 'fairy',    color: '#EF70EF' },
+];
